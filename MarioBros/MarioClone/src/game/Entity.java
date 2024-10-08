5975491a593c94a4c46b0a79b5cbd05a9bd9978d
6pernova <noahkuperman@gmail.com>
2024-10-08 20:38:09 -0300

    Entity, game

--- MarioBros/MarioClone/src/game/Entity.java
+++ MarioBros/MarioClone/src/game/Entity.java
@@ -2,4 +2,43 @@ package game;
 
 public abstract class Entity {
 
+    protected int x;
+    protected int y;
+    protected Sprite sprite;
+    protected Observer observer;
+
+    protected Entity(Sprite sprite, int x, int y){
+        this.sprite = sprite;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Sprite getSprite(){
+        return sprite;
+    }
+
+    public Sprite setSprite(Sprite sprite){
+        this.sprite = sprite;
+    }
+
+    public registerObserver(Observer observer){
+        this.observer = observer;
+    }
+
+    public setX(int x){
+        this.x = x;
+    }
+
+    public setY(int y){
+        this.y = y;
+    }
+
+    public getX(){
+        return x;
+    }
+
+    public getY(){
+        return y;
+    }
+
 }
--- MarioBros/MarioClone/src/game/Game.java
+++ MarioBros/MarioClone/src/game/Game.java
@@ -1,5 +1,28 @@
 package game;
 
+import factories.LevelGenerator;
+
 public class Game {
+    protected Player player;
+    protected LevelGenerator levelGenerator;
+    protected Level currentLevel;
+    protected ViewController viewController;
+    
+
+    public Game () {
+       levelGenerator = new LevelGenerator(new EntityFactory(new OriginalSpriteFactory()));
+       player = new Player();
+    }
+    
+    public setViewController(ViewController viewController){
+        this.viewController = viewController;
+    }
+
+    
+
+
+
+    
+
 
 }
