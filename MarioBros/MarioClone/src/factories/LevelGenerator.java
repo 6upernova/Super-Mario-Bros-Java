15be48464f424aeeabc6fbb51e32ab75c2fe2186
Maxi-PC <maxibarco.uns@gmail.com>
2024-10-12 17:51:40 -0300

    Arreglada la clase levelGenerator y modificada un poco la clase Level

--- MarioBros/MarioClone/src/factories/LevelGenerator.java
+++ MarioBros/MarioClone/src/factories/LevelGenerator.java
@@ -1,31 +1,44 @@
 package factories;
+import java.util.LinkedList;
+import java.util.List;
+
+import enemies.Enemy;
 import game.Entity;
+import platforms.Platform;
+import powerUps.PowerUp;
 
 public class LevelGenerator {
 
     protected EntityFactory entityFactory;
     protected Parser parser;
-    protected Level level;
 
     public LevelGenerator(String mode, int levelNumber) {
         entityFactory= new EntityFactory(mode);
         parser= new Parser(levelNumber);
-        level= new Level();
-        level.setBackGround(levelNumber);
     }
 
-    public void createLevel(int numberLevel){
+    public Level createLevel(int numberLevel){
         int type=0;
         int worldX=0;
         int worldY=0;
+        List<Platform> platformList = new LinkedList<Platform>();
+        List<Enemy> enemyList = new LinkedList<Enemy>();
+        List<PowerUp> powerUpList = new LinkedList<PowerUp>();
         //LEE CADA LINEA DEL TXT Y LO PASA A UN A CREATE, DESPUES CON ESO LO AÑADE
-            while(parser.hasToRead()) {
-                type= parser.getType();
-                worldX = parser.getPositionX();
-                worldY = parser.getPositionY();
-                Entity e= entityFactory.createEntity(type,worldX,worldY);
-                level.addEntity(e,e.getX(),e.getY());
-            }
+        while(parser.hasToRead()) {
+            type= parser.getType();
+            worldX = parser.getPositionX();
+            worldY = parser.getPositionY();
+            Entity e= entityFactory.createEntity(type,worldX,worldY);
+            
+            if( type>1 && type<9 ) //e
+                enemyList.add((Enemy) e);
+            else if( type>9 && type<15) //pwp
+                powerUpList.add((PowerUp) e);
+            else if( type>19 && type<25) //pltfrm
+                platformList.add((Platform) e);
+        }
+        return new Level(platformList, enemyList, powerUpList);
     }
 
 }
