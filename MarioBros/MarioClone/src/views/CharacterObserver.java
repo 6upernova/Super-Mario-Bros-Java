ad311279d2497329204a844026c8f7598b9c3fd5
6pernova <noahkuperman@gmail.com>
2024-10-12 17:34:54 -0300

    Character Observer

---
+++ MarioBros/MarioClone/src/views/CharacterObserver.java
@@ -0,0 +1,23 @@
+package views;
+
+import character.CharacterEntity;
+
+public class CharacterObserver extends GraphicObserver{
+
+    LevelScreen levelScreen;
+    CharacterEntity character;
+
+    public CharacterObserver(LevelScreen levelScreen, CharacterEntity character){
+        super(character);
+        update();
+    }
+
+    
+    public void update() {
+        super.update();
+        levelScreen.updateScrollRight(character);
+    }
+
+    
+
+}
