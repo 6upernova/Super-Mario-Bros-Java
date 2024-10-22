873518f36f8c08ce31153980a9afeb8c9f1a95be
Nicolas <nocigonza23.09@gmail.com>
2024-10-22 16:28:22 -0300

    Reproductor de sonido en game

---
+++ MarioBros/MarioClone/src/game/SoundReproducer.java
@@ -0,0 +1,54 @@
+package game;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JToolBar.Separator;
+
+public class SoundReproducer {
+
+	protected final String folderPath= "src"+ File.separator+"assets"+ File.separator+ "sounds" ;
+	protected Clip audioLevelMusic;
+	protected Clip auxialiarAudio;
+	
+	public SoundReproducer(String path) {
+		   try {
+				File file= new File(folderPath + File.separator + path + ".wav");
+		        audioLevelMusic= AudioSystem.getClip();
+		        audioLevelMusic.open(AudioSystem.getAudioInputStream(file));
+		} catch (Exception e) {
+			System.out.print(e.getMessage());
+		}
+	}
+	
+	public void setAuxiliarAudio(String path) {
+		File file= new File(folderPath + File.separator + path + ".wav");
+		try {
+	        auxialiarAudio= AudioSystem.getClip();
+			auxialiarAudio.open(AudioSystem.getAudioInputStream(file));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void start() {
+		if(!auxialiarAudio.isRunning()) {
+		   auxialiarAudio.setFramePosition(0);
+		   auxialiarAudio.start();
+		}
+	}
+	
+	public void loop() {
+		
+		audioLevelMusic.setFramePosition(0);
+		audioLevelMusic.loop(audioLevelMusic.LOOP_CONTINUOUSLY);
+	}
+	
+	public void stop() {
+		audioLevelMusic.stop();
+	}
+}
