f9eea924c9a51767a7ba6d678b84fb03b30fb557
Maxi-PC <maxibarco.uns@gmail.com>
2024-11-02 14:48:19 -0300

    Modificados los comentarios de la spriteFactory

--- CodigoFuente/src/factories/SpriteFactory.java
+++ CodigoFuente/src/factories/SpriteFactory.java
@@ -17,41 +17,38 @@ public abstract class SpriteFactory {
         return new Sprite(folderPath + File.separator+"Character"+number+".png");	 
     }
 
+    /*=========================================================Character Sprites=========================================================*/
+
     public Sprite getCharacterStillSprite(String direction){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Normal"+ File.separator + "CharacterStill"+ direction +".png");
     }
-    
     public Sprite getCharacterRightSprite(int frame) {
     	return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Normal"+ File.separator + "CharacterRight"+ frame + ".png");
     }
-    
     public Sprite getCharacterLeftSprite(int frame) {
     	return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Normal"+ File.separator + "CharacterLeft"+ frame + ".png");
     }
-
     public Sprite getCharacterJumpingSprite(String direction){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Normal"+ File.separator + "CharacterJumping" + direction + ".png");
     }
     public Sprite getCharacterInFlag(){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Normal"+ File.separator + "CharacterInFlag" + ".png");
     }
-    
     public Sprite getCharacterDead(){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Normal"+ File.separator + "CharacterDies" + ".png");
     }
-    ////////////////////////////////////////////////////////////////////////////////////////
+
+    /*======================================================Super Character Sprites======================================================*/
+
     public Sprite getSuperCharacterStillSprite(String direction){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Super"+ File.separator + "SuperCharacterStill"+ direction +".png");
     }
-    
     public Sprite getSuperCharacterRightSprite(int frame) {
     	return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Super"+ File.separator + "SuperCharacterRight"+ frame + ".png");
     }
-    
     public Sprite getSuperCharacterLeftSprite(int frame) {
     	return new Sprite(folderPath + File.separator + "Character" + File.separator+"Super"+ File.separator + "SuperCharacterLeft"+ frame + ".png");
     }
-
     public Sprite getSuperCharacterJumpingSprite(String direction){
         return new Sprite(folderPath + File.separator + "Character" + File.separator+"Super"+ File.separator + "SuperCharacterJumping" + direction + ".png");
     }
@@ -61,19 +58,18 @@ public abstract class SpriteFactory {
     public Sprite getSuperCharacterDead(){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Super"+ File.separator + "SuperCharacterDies" + ".png");
     }
-    ////////////////////////////////////////////////////////////////////////////////////////
+
+    /*======================================================Fire Character Sprites=======================================================*/
+
     public Sprite getFireCharacterStillSprite(String direction){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Fire"+ File.separator + "FireCharacterStill"+ direction +".png");
     }
-    
     public Sprite getFireCharacterRightSprite(int frame) {
     	return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Fire"+ File.separator + "FireCharacterRight"+ frame + ".png");
     }
-    
     public Sprite getFireCharacterLeftSprite(int frame) {
     	return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Fire"+ File.separator + "FireCharacterLeft"+ frame + ".png");
     }
-
     public Sprite getFireCharacterJumpingSprite(String direction){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Fire"+ File.separator + "FireCharacterJumping" + direction + ".png");
     }
@@ -83,7 +79,9 @@ public abstract class SpriteFactory {
     public Sprite getFireCharacterDead(){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Fire"+ File.separator + "FireCharacterDies" + ".png");
     }
-    ///////////////////////////INVENCIBLES NORMAL////////////////////////////////////
+
+    /*================================================Invencible Normal Character Sprites================================================*/
+
     public Sprite getCharacterInvencibleStillSprite(String direction){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator + "Invencible"+ File.separator + "Normal" + File.separator + "Invencible"+ direction +"Still.png");
     }
@@ -95,7 +93,6 @@ public abstract class SpriteFactory {
         	sprite = new Sprite(folderPath + File.separator+"Character"+ File.separator + "Normal"+ File.separator +"CharacterRight"+ frame + ".png");
         return sprite;
     }
-    
     public Sprite getCharacterInvencibleLeftSprite(int frame) {
         Sprite sprite;
         if(frame % 2 != 0)
@@ -104,18 +101,18 @@ public abstract class SpriteFactory {
         	sprite = new Sprite(folderPath + File.separator+"Character"+ File.separator + "Normal"+ File.separator +"CharacterLeft"+ frame + ".png");
         return sprite;
     }
-
     public Sprite getCharacterInvencibleJumpingSprite(String direction){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator + "Normal"+ File.separator +"CharacterJumping" + direction + ".png");
     }
     public Sprite getCharacterInvencibleDead(){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Normal"+ File.separator + "CharacterDies" + ".png");
     }
-    ///////////////////////////INVENCIBLES SUPER////////////////////////////////////
+    
+    /*==================================================Invencible Super Character Sprites=================================================*/
+
     public Sprite getSuperCharacterInvencibleStillSprite(String direction){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator + "Invencible"+ File.separator +"Super" +File.separator + "Invencible"+ direction +"Still.png");
     }
-    
     public Sprite getSuperCharacterInvencibleRightSprite(int frame) {
         Sprite sprite;
     	if(frame % 2 == 0)
@@ -124,7 +121,6 @@ public abstract class SpriteFactory {
         	sprite = new Sprite(folderPath + File.separator+"Character"+ File.separator + "Super"+ File.separator +"SuperCharacterRight"+ frame + ".png");
         return sprite;
     }
-    
     public Sprite getSuperCharacterInvencibleLeftSprite(int frame) {
         Sprite sprite;
         if(frame % 2 != 0)
@@ -133,23 +129,24 @@ public abstract class SpriteFactory {
         	sprite = new Sprite(folderPath + File.separator+"Character"+ File.separator + "Super"+ File.separator +"SuperCharacterLeft"+ frame + ".png");
         return sprite;
     }
-
     public Sprite getSuperCharacterInvencibleJumpingSprite(String direction){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator + "Super"+ File.separator +"SuperCharacterJumping" + direction + ".png");
     }
-    
     public Sprite getSuperCharacterInvencibleDead(){
         return new Sprite(folderPath + File.separator+"Character"+ File.separator+"Super"+ File.separator + "SuperCharacterDies" + ".png");
     }
 
-    //ENEMIES SPRITES
+    /*============================================================Goomba Sprites===========================================================*/
+
     public Sprite getGoombaLeftSprite(){
         return new Sprite(folderPath + File.separator+"Enemy"+ File.separator+"GoombaLeft.png");	
     }
     public Sprite getGoombaRightSprite(){
         return new Sprite(folderPath + File.separator+"Enemy"+ File.separator+"GoombaRight.png");	
     }
-    
+
+    /*=========================================================Koopa Troopa Sprites========================================================*/  
+
     public Sprite getKoopaTroopaLeftSprite(int frame){
         return new Sprite(folderPath + File.separator+"Enemy"+ File.separator+"KoopaTroopaLeft"+ frame + ".png");	 
     }
@@ -160,16 +157,22 @@ public abstract class SpriteFactory {
         return new Sprite(folderPath + File.separator+"Enemy"+ File.separator+"KoopaShell.png");	 
     }
 
+    /*========================================================Piranha Plant Sprites========================================================*/
+
     public Sprite getPiranhaPlantSprite(int frame){
         return new Sprite(folderPath + File.separator+"Enemy"+ File.separator+"PiranhaPlant"+ frame + ".png");	 
-    }    
-    
+    }  
+
+    /*============================================================Lakitu Sprites===========================================================*/ 
+
     public Sprite getLakituLeftSprite(){
         return new Sprite(folderPath + File.separator+"Enemy"+ File.separator+"LakituLeft.png");	
     }
     public Sprite getLakituRightSprite(){
         return new Sprite(folderPath + File.separator+"Enemy"+ File.separator+"LakituRight.png");	
     }
+
+    /*=========================================================Buzzy Beetle Sprites========================================================*/
 
     public Sprite getBuzzyBeetleLeftSprite(int frame){
         return new Sprite(folderPath + File.separator+"Enemy"+ File.separator+"BeetleLeft"+ frame + ".png");	 
@@ -181,13 +184,17 @@ public abstract class SpriteFactory {
         return new Sprite(folderPath + File.separator+"Enemy"+ File.separator+"BeetleShell.png");	 
     }
 
+    /*============================================================Spiny Sprites============================================================*/
+
     public Sprite getSpinySprite(){
         return new Sprite(folderPath + File.separator+"SpinnyLeft1.png");	 
     }
     public Sprite getShellSprite(){
         return new Sprite(folderPath + File.separator+"Shell.png");	 
     }
-    //POWERUPS SPRITES    
+    
+    /*==========================================================Power Ups Sprites==========================================================*/
+
     public Sprite getSuperMushroomSprite(){
         return new Sprite(folderPath + File.separator+"PowerUps"+ File.separator+"SuperMushroom.png");	 
     }
@@ -203,14 +210,18 @@ public abstract class SpriteFactory {
     public Sprite getCoinSprite(){
         return new Sprite(folderPath + File.separator+"PowerUps"+ File.separator+"Coin.png");	 
     }
-    //PROJETILE
+    
+    /*==========================================================Projectile Sprites=========================================================*/
+
     public Sprite getFireballSprite(){
         return new Sprite(folderPath + File.separator+"Projectiles"+ File.separator+"Fireball1.png");	 
     }
     public Sprite getSpinyEggSprite(){
         return new Sprite(folderPath + File.separator+"Projectiles"+ File.separator+"SpinyEgg.gif");	 
     }
-    //PLATFLORMS
+
+    /*==========================================================Platforms Sprites==========================================================*/
+
     public Sprite getBrickSprite(){
         return new Sprite(folderPath + File.separator+"Platforms"+ File.separator+"BrickBlock.png");	 
     }
@@ -247,4 +258,6 @@ public abstract class SpriteFactory {
     public Sprite getMastEndSprite() {
         return new Sprite(folderPath + File.separator+"Platforms"+ File.separator+"MastEnd.png");
     }
+
+    /*=====================================================================================================================================*/
 }
