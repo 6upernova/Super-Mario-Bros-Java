14c03eb7804bafade631a084b59174d78993a97d
6pernova <noahkuperman@gmail.com>
2024-11-05 20:57:11 -0300

    errores menores

--- CodigoFuente/src/entities/state/question/WithCoin.java
+++ CodigoFuente/src/entities/state/question/WithCoin.java
@@ -1,5 +1,6 @@
 package entities.state.question;
 import factories.Sprite;
+import observer.EntityObserver;
 import observer.SoundObserver;
 public class WithCoin extends QuestionState{
 
@@ -10,6 +11,7 @@ public class WithCoin extends QuestionState{
 	public int damaged(SoundObserver soundObserver) {
 		hits--;
 		soundObserver.reproduceSound("coin");
+		((EntityObserver)question.getGraphicObserver()).spawnCoin(question.coinAnimationSprites.get("1"));
 		if(hits==0)
 		   question.changeEmptyQuestionState();
 		return powerUpOn.getPoints();
